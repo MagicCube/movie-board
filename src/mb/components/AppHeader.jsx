f1ba9b9b3c9766a6e30f55b1f0a34068384511dd
Henry Li <henry1943@163.com>
2017-02-23 22:12:38 +0800

    Add App

---
+++ src/mb/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AppHeader() {
+  return (
+    <header className="mb-app-header">
+      <div className="mb-logo" />
+    </header>
+  );
+}
---
+++ src/mb/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppHeader from '../components/AppHeader';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="mb-app">
+        <AppHeader />
+      </div>
+    );
+  }
+}
