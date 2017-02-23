2c258cee32a49f6284c235a3fdc8a5fcc6c96bf6
Henry Li <henry1943@163.com>
2017-02-23 22:42:19 +0800

    Add translucent

--- src/mb/components/AppHeader.jsx
+++ src/mb/components/AppHeader.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export default function AppHeader() {
-  return (
-    <header className="mb-app-header">
-      <div className="mb-logo" />
-    </header>
-  );
+export default class AppHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      translucent: false
+    };
+  }
+
+  componentDidMount() {
+    $(window).on('scroll', () => {
+      this.setState({
+        translucent: document.body.scrollTop > 5
+      });
+    });
+  }
+
+  render() {
+    return (
+      <header className={`mb-app-header${this.state.translucent ? ' translucent' : ''}`}>
+        <div className="mb-logo" />
+      </header>
+    );
+  }
 }
