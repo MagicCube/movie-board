b55d7972a9ce33d7282b04bfee8daa303994f172
Henry Li <henry1943@163.com>
2017-02-24 22:17:19 +0800

    Add App-Footer

---
+++ src/mb/components/AppFooter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AppFooter() {
+  return (
+    <footer className="mb-app-footer">
+      <div className="copyright">© 2016-2017 <a target="_blank" href="https://github.com/MagicCube">MagicCube</a>. Designed and implemented by <a href="mailto:henry1943@163.com">Henry Li</a>.</div>
+      <a className="github" href="http://github.com/MagicCube/movie-board">Fork on Github</a>
+    </footer>
+  );
+}
