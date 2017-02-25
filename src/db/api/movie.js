50e2ef70965f389094d1f14bc017e3cd598c0b50
Henry Li <henry1943@163.com>
2017-02-25 13:06:40 +0800

    Refactor

--- src/db/api/movie.js
+++ src/db/api/movie.js
@@ -1,32 +1,8 @@
-const API_URL = 'http://api.douban.com/v2/movie';
-const API_KEY = '0df993c66c0c636e29ecbb5344252a4a';
-
+import { get } from './utils';
 
 export default {
-  inTheaters: createMethod('/data/in_theaters.json'),
-  comingSoon: createMethod('/data/coming_soon.json'),
-  top250: createMethod('top250')
+  comingSoon: get('/data/coming_soon.json'),
+  inTheaters: get('/data/in_theaters.json'),
+  subject: get(subjectId => `/api/movie/subject/${subjectId}`),
+  top250: get('top250'),
 };
-
-
-function fetch(path, args) {
-  const url = path.startsWith('/') ? path : `${API_URL}/${path}`;
-  return $.ajax({
-    url,
-    data: {
-      ...args,
-      apikey: API_KEY
-    }
-  });
-}
-
-function createMethod(
-  path,
-  payloadHandler = payload => payload,
-  responseHandler = response => response) {
-  return async (payload) => {
-    const args = payloadHandler(payload);
-    const response = await fetch(path, args);
-    return responseHandler(response);
-  };
-}
---
+++ src/db/api/utils.js
@@ -0,0 +1,39 @@
+const API_KEY = '0df993c66c0c636e29ecbb5344252a4a';
+
+function fetch(path, requiresAPIKey = false, args = {}) {
+  return $.ajax({
+    url: path,
+    data: {
+      ...args,
+      apiKey: requiresAPIKey ? API_KEY : undefined
+    }
+  });
+}
+
+/**
+ * Create a new GET resource.
+ *
+ * @param {string} path Path of resource.
+ * @param {bool} [false] Whether API Key is needed.
+ * @param {function} [payloadHandler=payload => payload] Handler of payload.
+ * @param {function} [responseHandler=response => response] Handler of response.
+ * @returns
+ */
+export function get(
+  path,
+  requiresAPIKey = false,
+  payloadHandler = payload => payload,
+  responseHandler = response => response) {
+  return async (payload) => {
+    let url = null;
+    if (typeof path === 'function') {
+      url = path(payload);
+    }
+    else {
+      url = path;
+    }
+    const args = payloadHandler(payload);
+    const response = await fetch(url, requiresAPIKey, args);
+    return responseHandler(response);
+  };
+}
