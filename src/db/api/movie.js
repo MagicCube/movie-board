c2c454d375e1d13f6d49fc358af21cfe1b2b059d
Henry Li <henry1943@163.com>
2017-02-26 16:38:54 +0800

    'GET Subject' method requires APIKEY to get more details.

--- src/db/api/movie.js
+++ src/db/api/movie.js
@@ -3,6 +3,6 @@ import { get } from './utils';
 export default {
   comingSoon: get('/data/coming_soon.json'),
   inTheaters: get('/data/in_theaters.json'),
-  subject: get(subjectId => `/api/movie/subject/${subjectId}`),
+  subject: get(subjectId => `/api/movie/subject/${subjectId}`, true),
   top250: get('top250'),
 };
