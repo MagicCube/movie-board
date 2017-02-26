777720030cfb28ecffad0ac661075e2d9fc08401
Henry Li <henry1943@163.com>
2017-02-26 11:02:05 +0800

    Remove property from AppHeader to make it 'purer'

--- src/mb/containers/App.jsx
+++ src/mb/containers/App.jsx
@@ -20,7 +20,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="mb-app">
-        <AppHeader navbars={[]} />
+        <AppHeader />
         {this.props.children}
         <AppFooter />
       </div>
