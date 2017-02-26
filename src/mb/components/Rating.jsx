80fde51889c7c3b360366324d11296e00c89861d
Henry Li <henry1943@163.com>
2017-02-26 16:07:24 +0800

    Add Rating

---
+++ src/mb/components/Rating.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import '../res/rating.less';
+
+/**
+ * The rating component.
+ */
+export default recompose.pure(({
+  rating: {
+    average,
+    stars
+  }
+}) => {
+  const starElements = [];
+  for (let i = 0; i < 50; i += 10) {
+    if (stars - i >= 10) {
+      starElements.push(<div className="star" key={i} />);
+    } else if (stars - i === 5) {
+      starElements.push(<div className="half star" key={i}><div className="inner-star" /></div>);
+    } else {
+      starElements.push(<div className="zero star" key={i} />);
+    }
+  }
+  return (
+    <div className="mb-rating" data-stars={stars}>
+      <div className="average">{average ? `${average} 分` : '暂无'}</div>
+      <div className="stars">
+        {starElements}
+      </div>
+    </div>
+  );
+});
