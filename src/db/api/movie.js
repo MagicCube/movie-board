8a1909f6a66a8dae45d72f0fa0b32d3212db9a98
Henry Li <henry1943@163.com>
2017-02-26 19:55:36 +0800

    Bugfix

--- src/db/api/movie.js
+++ src/db/api/movie.js
@@ -3,6 +3,6 @@ import { get } from './utils';
 export default {
   comingSoon: get('/data/coming_soon.json'),
   inTheaters: get('/data/in_theaters.json'),
-  subject: get(subjectId => `/api/movie/subject/${subjectId}`, true),
+  subject: get(subjectId => `/api/movie/subject/${subjectId}`, true, () => null),
   top250: get('top250'),
 };
