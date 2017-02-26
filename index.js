3a228ed4fb6dd0f724f70bca22fb69bd7cead334
Henry Li <henry1943@163.com>
2017-02-26 23:53:53 +0800

    Bugfix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
-const config = require('./webpack.config');
-
 const app = express();
 app.use(express.static('public'));
-app.use('/api', proxy(config.devServer.proxy['/api']));
+app.use('/api', proxy({
+  target: 'http://api.douban.com/v2',
+  pathRewrite: { '^/api': '' },
+  changeOrigin: true,
+}));
 app.listen(8080, () => {
   console.log('movie-board is now listening at 8080...');
 });
