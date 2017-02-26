336713d26db1261a2fbc4f5cab26fcce4119e5c7
Henry Li <henry1943@163.com>
2017-02-26 13:51:22 +0800

    Make AppFooter as 100% pure component

--- src/mb/components/AppFooter.jsx
+++ src/mb/components/AppFooter.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { pure } from 'recompose';
 
 import '../res/app-footer.less';
 
 /**
  * Footer component of application.
  */
-export default pure(() => (
+export default recompose.pure(() => (
   <footer className="mb-app-footer">
     <div className="copyright">© 2016-2017 <a rel="noopener noreferrer" target="_blank" href="https://github.com/MagicCube">MagicCube</a>. Designed and implemented by <a href="mailto:henry1943@163.com">Henry Li</a>.</div>
     <a className="github" href="http://github.com/MagicCube/movie-board">Fork on Github</a>
