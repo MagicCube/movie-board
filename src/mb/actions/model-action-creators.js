b7abf6c443e2a15aa91fba17f5eb47bb90b6de7f
Henry Li <henry1943@163.com>
2017-02-26 19:56:03 +0800

    Bugfix

--- src/mb/actions/model-action-creators.js
+++ src/mb/actions/model-action-creators.js
@@ -5,5 +5,5 @@ import movieApi from '../../db/api/movie';
 export default {
   loadInTheaters: createAction('LOAD_IN_THEATERS', movieApi.inTheaters),
   loadComingSoon: createAction('LOAD_COMING_SOON', movieApi.comingSoon),
-  loadSubject: createAction('LOAD_SUBJECT', movieApi.loadSubject)
+  loadSubject: createAction('LOAD_SUBJECT', movieApi.subject)
 };
