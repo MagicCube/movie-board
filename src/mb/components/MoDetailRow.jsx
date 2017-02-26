c58044f46cd5cf309b78f1fca3d41a769de36328
Henry Li <henry1943@163.com>
2017-02-26 19:57:21 +0800

    Change to Component with customized shouldComponentUpdate()

--- src/mb/components/MoDetailRow.jsx
+++ src/mb/components/MoDetailRow.jsx
@@ -1,3 +1,4 @@
+import Immutable from 'immutable';
 import React from 'react';
 
 import '../res/mo-detail-row.less';
@@ -11,25 +12,34 @@ const TYPES = {
 /**
  * Represent a inline list in a detail row of MoJumbotron.
  */
-export default recompose.pure(({
-  data,
-  type
-}) => {
-  let items = null;
-  if (type === 'genres') {
-    items = data.map(genre => <li key={genre}>{genre}</li>);
-  } else {
-    items = data.map(people => <li key={people.get('id')}>{people.get('name')}</li>);
+export default class MoDetailRow extends React.Component {
+  static propTypes = {
+    data: React.PropTypes.objectOf(Immutable.List).isRequired,
+    type: React.PropTypes.string.isRequired
   }
 
-  return (
-    <dl className={`mb-mo-detail-row ${type}`}>
-      <dt>{TYPES[type]}</dt>
-      <dd>
-        <ul>
-          {items}
-        </ul>
-      </dd>
-    </dl>
-  );
-});
+  shouldComponentUpdate(nextProps) {
+    return nextProps.type !== this.props.type || !nextProps.data.equals(this.props.data);
+  }
+
+  render() {
+    const { data, type } = this.props;
+    let items = null;
+    if (type === 'genres') {
+      items = data.map(genre => <li key={genre}>{genre}</li>);
+    } else {
+      items = data.map(people => <li key={people.get('id')}>{people.get('name')}</li>);
+    }
+
+    return (
+      <dl className={`mb-mo-detail-row ${type}`}>
+        <dt>{TYPES[type]}</dt>
+        <dd>
+          <ul>
+            {items}
+          </ul>
+        </dd>
+      </dl>
+    );
+  }
+}
