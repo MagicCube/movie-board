6e362f491d3b59d50e8742261128fdd0b707bd5c
Henry Li <henry1943@163.com>
2017-02-26 13:37:56 +0800

    Add rel to <A/>

--- src/mb/components/AppFooter.jsx
+++ src/mb/components/AppFooter.jsx
@@ -8,7 +8,7 @@ import '../res/app-footer.less';
 export default function AppFooter() {
   return (
     <footer className="mb-app-footer">
-      <div className="copyright">© 2016-2017 <a target="_blank" href="https://github.com/MagicCube">MagicCube</a>. Designed and implemented by <a href="mailto:henry1943@163.com">Henry Li</a>.</div>
+      <div className="copyright">© 2016-2017 <a rel="noopener noreferrer" target="_blank" href="https://github.com/MagicCube">MagicCube</a>. Designed and implemented by <a href="mailto:henry1943@163.com">Henry Li</a>.</div>
       <a className="github" href="http://github.com/MagicCube/movie-board">Fork on Github</a>
     </footer>
   );
