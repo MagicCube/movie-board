540f19758bd55e1a26b0a133ac46ee2100bdc41e
Henry Li <henry1943@163.com>
2017-02-27 11:14:20 +0800

    Add a copy of modelsActionCreators.loadSubject()

--- src/mb/actions/lolomo-action-creators.js
+++ src/mb/actions/lolomo-action-creators.js
@@ -1,5 +1,8 @@
 import { createAction } from 'redux-actions';
 
+import modelActionCreators from './model-action-creators';
+
 export default {
-  selectSubject: createAction('SELECT_SUBJECT')
+  selectSubject: createAction('SELECT_SUBJECT'),
+  loadSubject: modelActionCreators.loadSubject
 };
