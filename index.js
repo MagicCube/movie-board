f246de9593eaf980be7a8fda7679c2b785f6e042
Henry Li <henry1943@163.com>
2017-02-28 17:25:39 +0800

    Add WARNING

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use('/api', proxy({
   pathRewrite: { '^/api': '' },
   changeOrigin: true,
 }));
+
+console.log('\n[WARNING] movie-board is now running in PRODUCTION mode.');
+console.log('[WARNING] If you want to run it in DEVELOPMENT mode, please try "npm run dev" instead.\n');
 app.listen(8080, () => {
-  console.log('movie-board is now listening at 8080...');
+  console.log('[INFO] movie-board is now listening at 8080...');
 });
