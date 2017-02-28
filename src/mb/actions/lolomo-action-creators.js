b8f10c09db03493b06e0e8061b5c39be1cabe64a
Henry Li <henry1943@163.com>
2017-02-28 14:43:59 +0800

    Add selectTab

--- src/mb/actions/lolomo-action-creators.js
+++ src/mb/actions/lolomo-action-creators.js
@@ -4,5 +4,6 @@ import modelActionCreators from './model-action-creators';
 
 export default {
   selectSubject: createAction('SELECT_SUBJECT'),
+  selectTab: createAction('SELECT_TAB'),
   loadSubject: modelActionCreators.loadSubject
 };
--- src/mb/reducers/lolomo.js
+++ src/mb/reducers/lolomo.js
@@ -5,7 +5,10 @@ import actionCreators from '../actions/lolomo-action-creators';
 
 const initialState = Immutable.fromJS({
   selectedSubjectId: null,
-  selectedRowKey: null
+  selectedRowKey: null,
+  tabs: {
+    selectedTabId: 'general'
+  }
 });
 
 export default handleActions({
@@ -14,9 +17,15 @@ export default handleActions({
       const { subject, rowKey } = selection;
       return state.merge({
         selectedSubjectId: subject ? subject.get('id') : null,
-        selectedRowKey: subject ? rowKey : null
+        selectedRowKey: subject ? rowKey : null,
+        tabs: {
+          selectedTabId: 'general'
+        }
       });
     }
     return initialState;
+  },
+  [actionCreators.selectTab](state, { payload: tabId }) {
+    return state.setIn(['tabs', 'selectedTabId'], tabId);
   }
 }, initialState);
