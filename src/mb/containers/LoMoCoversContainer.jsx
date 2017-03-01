f246fae933bbaf35729050802f5863b4dd26e6c1
Henry Li <henry1943@163.com>
2017-03-01 23:07:21 +0800

    Add LoMoCoversContainer

---
+++ src/mb/containers/LoMoCoversContainer.jsx
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import React from 'react';
+
+import LoMoCovers from '../components/LoMoCovers';
+
+import actionCreators from '../actions/lolomo-action-creators';
+
+
+export default connect(
+  (state, ownProps) => ({
+    subjects: state.getIn(['models', ownProps.modelKey, 'subjects']),
+    selectedSubjectId: state.getIn(['lolomo', 'selectedSubjectId']),
+    hasSelection: state.getIn(['lolomo', 'selectedModelKey']) === ownProps.modelKey
+  }),
+  (dispatch, ownProps) => ({
+    actions: {
+      selectSubject(subject) {
+        dispatch(actionCreators.selectSubject({ subject, modelKey: ownProps.modelKey }));
+        if (subject) {
+          dispatch(actionCreators.loadSubject(subject.get('id')));
+        }
+      }
+    }
+  })
+)(LoMoCovers);
