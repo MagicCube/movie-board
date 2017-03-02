0ce0f54d5a3a7dc4e6e906347a58762d5414bb8f
Henry Li <henry1943@163.com>
2017-03-02 13:14:02 +0800

    Add dev log

--- src/mb/index.jsx
+++ src/mb/index.jsx
@@ -19,4 +19,14 @@ $(() => {
     </Provider>,
     document.getElementById('mbMountPoint')
   );
+
+  try {
+    console.log('%c', 'padding: 20px 300px; line-height: 50px; background:url(http://henryli-movie-board.daoapp.io/assets/images/mb-logo.png) no-repeat;');
+  } catch (e) {
+
+  }
+  console.info('WELCOME ABOARD, DEVELOPERS!');
+  console.info('Feel free to send me email via henry1943@163.com');
+  console.info('And don\'t forget to star my project on Github :)');
+  console.info('https://github.com/MagicCube/movie-board/');
 });
