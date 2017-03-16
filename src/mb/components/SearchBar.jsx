41d1608c044648c7e934d649a4017b5e2606525d
Henry Li <henry1943@163.com>
2017-03-16 16:41:11 +0800

    Add onKeywordChange event

--- src/mb/components/SearchBar.jsx
+++ src/mb/components/SearchBar.jsx
@@ -3,14 +3,29 @@ import React from 'react';
 import '../res/search-bar.less';
 
 export default class SearchBar extends React.Component {
-  onChange(e) {
-    console.log(e);
+  static propTypes = {
+    onKeywordChange: React.PropTypes.func.isRequired
+  }
+
+  handleChange(e) {
+    const value = e.target.value.trim();
+    this.clearChangingTimer();
+    this.changingTimer = setTimeout(() => {
+      this.props.onKeywordChange({ keyword: value !== '' ? value : null });
+    }, 360);
+  }
+
+  clearChangingTimer() {
+    if (this.changingTimer) {
+      clearTimeout(this.changingTimer);
+    }
+    this.changingTimer = null;
   }
 
   render() {
     return (
       <div className="mb-search-bar">
-        <input type="search" onChange={e => this.onChange(e)} placeholder="影片搜索" />
+        <input type="search" onChange={e => this.handleChange(e)} placeholder="影片搜索" />
         <span className="octicon icon-search" />
       </div>
     );
