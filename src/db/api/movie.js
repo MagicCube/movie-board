883c62674e8ab38c4e3523c1bf68fcc49487033d
Henry Li <henry1943@163.com>
2017-03-23 11:00:13 +0800

    Add search API

--- src/db/api/movie.js
+++ src/db/api/movie.js
@@ -3,6 +3,7 @@ import { get } from './utils';
 export default {
   comingSoon: get('/data/coming_soon.json'),
   inTheaters: get('/data/in_theaters.json'),
+  search: get('/api/movie/search', undefined, payload => ({ q: payload.query })),
   subject: get(subjectId => `/api/movie/subject/${subjectId}`, { requiresAPIKey: true, cache: sessionStorage }, () => null),
   top20: get('/data/top20.json')
 };
