89d6523610ba3080bcc8eb1d3d543056f80fdecb
Henry Li <henry1943@163.com>
2017-03-23 11:03:26 +0800

    Add query handler and dispatch mapping

--- src/mb/containers/App.jsx
+++ src/mb/containers/App.jsx
@@ -1,4 +1,6 @@
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import Immutable from 'immutable';
 import React from 'react';
 
@@ -6,6 +8,7 @@ import AppFooter from '../components/AppFooter';
 import AppHeader from '../components/AppHeader';
 import ProgressBar from '../components/ProgressBar';
 import SearchBar from '../components/SearchBar';
+import modelActionCreators from '../actions/model-action-creators';
 
 import '../res/app.less';
 
@@ -15,20 +18,37 @@ import '../res/app.less';
 @connect(
   state => ({
     status: state.get('status')
+  }),
+  dispatch => ({
+    actions: bindActionCreators(modelActionCreators, dispatch)
   })
 )
 export default class App extends React.PureComponent {
   static propTypes = {
     status: React.PropTypes.objectOf(Immutable.Map).isRequired,
-    children: React.PropTypes.element
+    children: React.PropTypes.element,
+    actions: React.PropTypes.shape({
+      search: React.PropTypes.func.isRequired
+    }).isRequired
   }
 
   static defaultProps = {
     children: []
   }
 
+  handleQueryChange(e) {
+    if (e.query !== null) {
+      if (browserHistory.getCurrentLocation().pathname !== '/search') {
+        browserHistory.push('/search');
+      }
+      this.props.actions.search({ query: e.query });
+    } else if (browserHistory.getCurrentLocation().pathname !== '/') {
+      browserHistory.push('/');
+    }
+  }
+
   render() {
-    this.searchBar = <SearchBar onKeywordChange={e => console.log(e.keyword)} />;
+    this.searchBar = <SearchBar onQueryChange={e => this.handleQueryChange(e)} />;
     return (
       <div className="mb-app">
         <AppHeader navbars={this.searchBar} />
