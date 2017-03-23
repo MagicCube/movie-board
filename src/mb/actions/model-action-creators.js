aa8de957e023608a912f0212b77709b8ace94bd1
Henry Li <henry1943@163.com>
2017-03-23 11:00:48 +0800

    Add search action

--- src/mb/actions/model-action-creators.js
+++ src/mb/actions/model-action-creators.js
@@ -3,8 +3,9 @@ import { createAction } from 'redux-actions';
 import movieApi from '../../db/api/movie';
 
 export default {
-  loadInTheaters: createAction('LOAD_IN_THEATERS', movieApi.inTheaters),
   loadComingSoon: createAction('LOAD_COMING_SOON', movieApi.comingSoon),
+  loadInTheaters: createAction('LOAD_IN_THEATERS', movieApi.inTheaters),
+  loadSubject: createAction('LOAD_SUBJECT', movieApi.subject),
   loadTop20: createAction('LOAD_TOP_20', movieApi.top20),
-  loadSubject: createAction('LOAD_SUBJECT', movieApi.subject)
+  search: createAction('SEARCH', movieApi.search)
 };
