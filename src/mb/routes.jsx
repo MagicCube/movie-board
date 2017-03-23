7ad02fc42ed41b286aae66fbba006deb0451080c
Henry Li <henry1943@163.com>
2017-03-23 13:52:42 +0800

    Move App.jsx to 'app' folder

--- src/mb/routes.jsx
+++ src/mb/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 
-import App from './containers/App';
+import App from './app/App';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 
